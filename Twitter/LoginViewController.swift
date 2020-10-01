ca76f8ec478a5ea7db4114a5d355eebb10881d81
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-09-30 23:15:51 -0400

    Implemented a user story that sllows to stay logged in across restarts

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -12,14 +12,21 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
     }
     
     @IBAction func onLoginButton(_ sender: Any) {
         
         let loginUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: loginUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not log in!")
